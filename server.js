dc9c279c7e1297f7f92fdc46bff700922697e7bb
linkatoh22 <lintoh111@gmail.com>
2024-05-25 23:31:56 +0700

    add data connection;

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express =require("express");
+const connectDb=require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv=require("dotenv").config();
 const app= express();
 
+connectDb();
 const port =process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
