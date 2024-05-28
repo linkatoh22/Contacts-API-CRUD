5e5f245d203e7dc438de205b2957f08d83a6e135
linkatoh22 <lintoh111@gmail.com>
2024-05-28 15:31:52 +0700

    add login

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,43 @@
 //@desc Register
 //route GET /api/users/register
 //@acess public everyone can entry
-
+const bcrypt= require ("bcrypt");
 const asyncHandler = require("express-async-handler");
-
+const User = require("../models/userModel");
 const registerUser = asyncHandler (async (req,res)=>{
+    const {username, email,password}=req.body;
+    if(!username||!email||!password)
+        {
+            res.status(400);
+            throw new Error ("All fields are mandatory !");
+        }
+    const userAvailable = await User.findOne({email});
+    if (userAvailable)
+        {
+            res.status(400);
+            throw new Error ("User already registed!");
+        }
+    //Hash password
+    const hashedPassword= await bcrypt.hash(password,10); //10 solve round
+    console.log("Hashed password: ",hashedPassword);
+    
+
+    const user = await User.create ({
+        username,
+        email,
+        password:hashedPassword
+    });
+    console.log("User created "+user);
+    if (user)
+        {
+            res.status(201).json({_id:user.id,email:user.email });
+        }
+    else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({message:"Register the user"});
+
 });
 
 //@desc Login User
